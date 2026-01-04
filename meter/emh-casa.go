ff65c854175c2b454ead62f5fe970c6f4b61e815
Ingo <71161062+iseeberg79@users.noreply.github.com>
2026-01-05 00:07:55 +0100

    Update emh-casa.go

--- meter/emh-casa.go
+++ meter/emh-casa.go
@@ -1,29 +1,5 @@
-// Package emhcasa provides EMH CASA 1.1 Smart Meter Gateway support.
-// Based on work by gosanman (https://github.com/gosanman/smartmetergateway)
-//
-// MIT License
-//
-// Copyright (c) 2026 gosanman
-// Copyright (c) 2026 iseeberg79
-// SPDX-License-Identifier: MIT
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// provides EMH CASA 1.1 Smart Meter Gateway support
+// This is a wrapper around the emh-casa-go library:
 
 package meter
 
@@ -33,7 +9,7 @@ import (
 
 	"github.com/evcc-io/evcc/api"
 	"github.com/evcc-io/evcc/util"
-	"github.com/iseeberg79/emh-casa-go"
+	emhcasa "github.com/iseeberg79/emh-casa-go"
 )
 
 func init() {
